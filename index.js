6ffc5e304c6ea6d3277ed527391facbfe2ee3e54
Simon Willcock <simon@willcock.com.au>
2017-10-25 10:51:46 +1100

    Add console log for summary destination

--- index.js
+++ index.js
@@ -6,10 +6,18 @@ const fs = require('fs'),
 function generateEntry( title, path, readmeFilename ) {
   let depth = path.match(/\//g).length
 
-  if ( path.indexOf( readmeFilename ) == -1 )
+  if ( path.indexOf( readmeFilename ) === -1 )
     depth++
 
+  if (depth === 1){
+    return generateSection(title);
+  }
+
   return `${Array(depth).join('    ')}- [${title}](${path})\n`
+}
+
+function generateSection( title , sectionFilename) {
+  return `\n## ${title}\n`
 }
 
 module.exports = {
@@ -18,7 +26,8 @@ module.exports = {
       const root = this.resolve(''),
             bookTitle = this.config.get('title'),
             readmeFilename = this.config.get('structure.readme'),
-            summaryFilename = this.config.get('structure.summary')
+            sectionFilename = this.config.get('pluginConfig.summary.section'),
+            summaryFilename = this.config.get('structure.summary');
 
       glob(
         `*/**/*.md`,
@@ -27,18 +36,21 @@ module.exports = {
         },
         ( err, files ) => {
           let summaryContent = ( bookTitle ? `# ${bookTitle}\n\n` : '' )
-
+          console.log(sectionFilename)
           files.forEach( ( filePath ) => {
             if (filePath.indexOf("node_modules") === -1)
             {
               console.log(`Parsing: ${root}/${filePath}`);
               const markdown = jsonMark.parse( fs.readFileSync( `${root}/${filePath}`, { encoding: 'utf8' } ) ),
-                    fileTitle = markdown.order[0]
-
-              summaryContent += generateEntry( fileTitle, filePath, readmeFilename )
+                    fileTitle = markdown.order[0] || filePath;
+              if ( filePath.indexOf( sectionFilename ) !== -1  && 1 == 2) {
+                summaryContent += generateSection( fileTitle, sectionFilename );
+              } else {
+                summaryContent += generateEntry( fileTitle, filePath, readmeFilename );
+              }
             }
           })
-
+          console.log(`${root}/${summaryFilename}`);
           fs.writeFileSync( `${root}/${summaryFilename}`, summaryContent, { encoding: 'utf8' } )
 
           console.log(`\x1b[36mgitbook-plugin-summary: \x1b[32m${summaryFilename} generated successfully.`)
